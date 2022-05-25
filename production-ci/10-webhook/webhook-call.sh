b3cd7929f35c9f0fff622a6ae5043514849057e5
Mark Roberts <marrober@redhat.com>
2022-05-25 11:34:53 +0100

    pre-production testing

---
+++ production-ci/10-webhook/webhook-call.sh
@@ -0,0 +1,10 @@
+{
+  "repository": "marrober/myapp",
+  "namespace": "marrober",
+  "name": "myapp",
+  "docker_url": "quay.io/marrober/myapp",
+  "homepage": "https://quay.io/repository/marrober/myapp",
+  "updated_tags": [
+    "6322-phcb2"
+  ]
+}
