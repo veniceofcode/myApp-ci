80985a92a00122d5629407bf53055d60d721383a
Mark Roberts <marrober@redhat.com>
2022-05-26 16:59:32 +0100

    Adding spoof call to production deployment webhook.

---
+++ production-ci/04-webhook/webhook-call.sh
@@ -0,0 +1,1 @@
+curl -k -s -d "{\"repository\": \"marrober/myapp\", \"namespace\": \"marrober\", \"name\": \"myapp\", \"docker_url\": \"quay.io/marrober/myapp\", \"homepage\": \"https://quay.io/repository/marrober/myapp\", \"updated_tags\": [\"6322-phcb2\"]}" http://myapp-github-ci-prod-listener-myapp-ci.apps.skylake.demolab.local
